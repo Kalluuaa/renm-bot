dc4675f28a18d624c7e13cc1c88c07bf492829f3
Kallluaa <136989645+Kallluaa@users.noreply.github.com>
2023-11-12 13:41:18 +0530

    Update cb_data.py

--- plugins/cb_data.py
+++ plugins/cb_data.py
@@ -155,7 +155,7 @@ async def vid(bot,update):
      total_used = used + int(file.file_size)
      used_limit(update.from_user.id,total_used)
      try:
-     		path = await bot.download_media(message = file, progress=progress_for_pyrogram,progress_args=( "``` Trying To Download...```",  ms, c_time   ))
+     		path = await bot.download_media(message = file, progress=progress_for_pyrogram,progress_args=( "``` 𝗧𝗿𝘆𝗶𝗻𝗴 𝗧𝗼 𝗗𝗼𝘄𝗻𝗹𝗼𝗮𝗱...```",  ms, c_time   ))
      		
      except Exception as e:
           neg_used = used - int(file.file_size)
