33d3319fedc43c0936c4c705a427cc9510a54685
Kallluaa <136989645+Kallluaa@users.noreply.github.com>
2023-11-12 17:43:42 +0530

    Update cb_data.py

--- plugins/cb_data.py
+++ plugins/cb_data.py
@@ -149,7 +149,7 @@ async def vid(bot,update):
      file_path = f"downloads/{new_filename}"
      message = update.message.reply_to_message
      file = message.document or message.video or message.audio
-     ms = await update.message.edit("```Trying To Download...```")
+     ms = await update.message.edit("```𝗧𝗿𝘆𝗶𝗻𝗴 𝗧𝗼 𝗗𝗼𝘄𝗻𝗹𝗼𝗮𝗱...```")
      used_limit(update.from_user.id,file.file_size)
      c_time = time.time()
      total_used = used + int(file.file_size)
