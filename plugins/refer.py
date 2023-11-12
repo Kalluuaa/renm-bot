ad8efab034759c18d883cd2e3296f6050011f63b
Kallluaa <136989645+Kallluaa@users.noreply.github.com>
2023-11-12 13:15:33 +0530

    Update refer.py

--- plugins/refer.py
+++ plugins/refer.py
@@ -3,6 +3,6 @@ from pyrogram.types import ( InlineKeyboardButton, InlineKeyboardMarkup,ForceRep
 @Client.on_message(filters.private & filters.command(["refer"]))
 async def refer(client,message):
     reply_markup = InlineKeyboardMarkup(
-       		[[ InlineKeyboardButton("𝐒𝐡𝐚𝐫𝐞 𝐘𝐨𝐮𝐫 𝐋𝐢𝐧𝐤" ,url=f"https://t.me/share/url?url=https://t.me/Public_renamebot?start={message.from_user.id}") ]   ])
+       		[[ InlineKeyboardButton("🔗 𝐒𝐡𝐚𝐫𝐞 𝐘𝐨𝐮𝐫 𝐋𝐢𝐧𝐤" ,url=f"https://t.me/share/url?url=https://t.me/Public_renamebot?start={message.from_user.id}") ]   ])
     await message.reply_text(f"Rᴇғᴇʀ Aɴᴅ Eᴀʀɴ Gᴇᴛ 𝟷𝟶𝟶MB Uᴘʟᴏᴀᴅ Lɪᴍɪᴛ \nPᴇʀ Rᴇғᴇʀ 𝟷𝟶𝟶MB\n Yᴏᴜʀ Lɪɴᴋ :- https://t.me/Public_renamebot?start={message.from_user.id} ",reply_to_message_id = message.id,reply_markup=reply_markup,)
     
